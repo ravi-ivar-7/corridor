7eaabf4c891f476005b6d45a018f5c3b76edacd9
Ravi Kumar <ravikumar117211@gmail.com>
2025-10-26 23:18:30 +0530

    Remove choco-master directory from Git

--- choco-master/extension/dashboard/types.d.ts
+++
@@ -1,9 +0,0 @@
-// Type declarations for Chrome extension dashboard
-
-interface Window {
-    dashboardController?: DashboardController;
-}
-
-interface DashboardController {
-    selectPlatform(platform: any): void;
-}
--- choco-master/extension/lib/config/constants.js
+++
@@ -1,3 +0,0 @@
-class Constants {
-    static BACKEND_URL =  'https://usechoco.vercel.app/' //  'http://localhost:3000/' // 
-}
